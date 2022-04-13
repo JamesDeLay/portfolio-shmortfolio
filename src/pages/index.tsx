38aa8e8cc691f83ae00de14fee38703c7a40851d
James DeLay <jdelay.jr@gmail.com>
2022-04-12 23:13:08 -0400

    feat: adding MUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import Title from '@/components/Title';
+import { Box, Button } from '@mui/material';
 
 export default function Home() {
   return (
     <main>
       <Title>Hello TypeScript!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
+      <Box p={4}>
+        <Button variant="contained">Hello gatsby-theme-material-ui</Button>
+      </Box>
     </main>
   );
 }
